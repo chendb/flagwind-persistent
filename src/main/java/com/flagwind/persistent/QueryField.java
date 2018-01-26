a11ccde012d8b50434740f815b6cda1911edfc78
chendebao <chendebao1985@163.com>
2018-01-26 14:56:35 +0800

    修改查询字段定义

---
+++ src/main/java/com/flagwind/persistent/QueryField.java
@@ -0,0 +1,40 @@
+package com.flagwind.persistent;
+
+public class AggregateField {
+    private String column;
+    private String alias;
+    private AggregateType type;
+    private Class<?> javaType;
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public AggregateType getType() {
+        return type;
+    }
+
+    public void setType(AggregateType type) {
+        this.type = type;
+    }
+
+    public Class<?> getJavaType() {
+        return javaType;
+    }
+
+    public void setJavaType(Class<?> javaType) {
+        this.javaType = javaType;
+    }
+}
