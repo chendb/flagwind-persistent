308a51a74e3421fda4b5926dec8481e3fbc3e1b9
chendebao <chendebao1985@163.com>
2018-03-09 14:03:15 +0800

    增加多重注解支持

--- src/main/java/com/flagwind/persistent/annotation/ConditionOperator.java
+++ src/main/java/com/flagwind/persistent/annotation/ConditionOperator.java
@@ -2,8 +2,7 @@ package com.flagwind.persistent.annotation;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -11,14 +10,14 @@ import java.lang.annotation.Target;
 import com.flagwind.persistent.model.ClauseOperator;
 
 
-
+@Repeatable(ConditionOperators.class)
 @Retention(RetentionPolicy.RUNTIME)
-@Target({  METHOD, FIELD })
+@Target({ METHOD, FIELD })
 public @interface ConditionOperator {
-
+	
 
 	String name() default "";
-
+	
 
 	ClauseOperator operator() default ClauseOperator.Equal;
 }
